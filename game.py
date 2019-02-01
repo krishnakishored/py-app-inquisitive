590ce38e41a61431b89f675db4fef07716a7a9ec
kishore.donthikurthi <kishore.donthikurthi@ispace.com>
2019-02-01 16:11:43 +0530

    command line options

--- game.py
+++ game.py
@@ -126,29 +126,64 @@ def report_card_app(database,table,word_list):
     print('wrong={0},right={1},skip={2} in {3}'.format(wrong,right,skip, table))
     capture_results(database,table,word_list)
 
-if __name__ == "__main__":
-    database = './data/sqlite3/inquisitive.db'
-    
-    game_mode = "quiz"
 
-    if(game_mode=='quiz'):
-        '''
-        quiz based on random selection
-        '''
-        table = 'noun'
-        num_of_questions = 3
-        # run_quiz_app(database,table,num_of_questions,"english")
-        run_quiz_app(database,table,num_of_questions,"german")
-    else:    
-        '''
-        interactive 
-        # a specific query with 'where' clause to select the choice of questions
-        '''
-        table= 'noun' # can specify a partsofspeech table name or master
-        num_of_questions = 3
+
+import argparse
+
+def run_inquisitive_with_args(*args,**kwargs):
+    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='inquisitive app')
+    
+    parser.add_argument("-m","--mode", help="choose (i)nteractive or (q)uiz ",default="q")
+    parser.add_argument("-t","--type", help="choose parts of speech - sentence, verb, noun, preposition, .. ",default="sentence")
+    parser.add_argument("-n","--count", help="choose no.of questions",type=int,default=5)
+    parser.add_argument("-db","--database", help="select the database",default='./data/sqlite3/inquisitive.db')
+    args = parser.parse_args()
+
+
+    # table = args['type']
+    table = args.type
+    num_of_questions = args.count
+    database = args.database
+
+    print(args)
+    if(args.mode=='i'): #     interactive : a specific query with 'where' clause to select the choice of questions
         #  select_query= "SELECT german_word,english_word FROM {0} LIMIT {1}".format(table,num_of_questions)
         select_query= "SELECT german_word,english_word FROM {0} ORDER BY RANDOM() LIMIT {1}".format(table,num_of_questions)
         interactive_console_app(database, table,num_of_questions,guess="english", sql_statement=select_query)
-        
+    else:
+         #quiz based on random selection
+        # run_quiz_app(database,table,num_of_questions,"english")
+        run_quiz_app(database,table,num_of_questions,"german")
+    
 
+
+if __name__ == "__main__":
+    run_inquisitive_with_args()
+
+
+    # #defaults
+    # database = './data/sqlite3/inquisitive.db'
+    # game_mode = "quiz"
+    # table= 'noun' # can specify a partsofspeech table name or master
+    # num_of_questions = 3
+
+    
+
+    # if(game_mode=='quiz'):
+    #     '''
+    #     quiz based on random selection
+    #     '''
+    #     # run_quiz_app(database,table,num_of_questions,"english")
+    #     run_quiz_app(database,table,num_of_questions,"german")
+    # else:    
+    #     '''
+    #     interactive 
+    #     # a specific query with 'where' clause to select the choice of questions
+    #     '''
+    #     #  select_query= "SELECT german_word,english_word FROM {0} LIMIT {1}".format(table,num_of_questions)
+    #     select_query= "SELECT german_word,english_word FROM {0} ORDER BY RANDOM() LIMIT {1}".format(table,num_of_questions)
+    #     interactive_console_app(database, table,num_of_questions,guess="english", sql_statement=select_query)
+        
+    
    
